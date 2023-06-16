c0836cc8c8fe880e440e427585487cc8e71c06c8
gourav-mishra551 <gouravmishra200105@gmail.com>
2023-06-16 17:04:36 +0530

    first commit

---
+++ client/src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    backendUrl: "http://localhost:8080/api/v1/dalle",
+  },
+  production: {
+    backendUrl: "https://threedai-tshirtdesign.onrender.com",
+  },
+};
+
+export default config;
